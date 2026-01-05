93b92ac2692736df46a5cd8fd90219b2f6537a01
Ahmed Ayman <154859694+AhmedAymannn@users.noreply.github.com>
2026-01-05 15:18:56 +0200

    Add file storage and validation utilities for resumes
    Added FileStorageUtil and FileValidationUtil classes to handle file storage and validation logic for resumes. Updated Resume entity to include fileName and fileSize fields for better file metadata tracking.

---
+++ src/main/java/Ahmed/com/JobSync/utils/StrageUtils/FileStorageUtil.java
@@ -0,0 +1,25 @@
+package Ahmed.com.JobSync.utils.StrageUtils;
+
+
+//
+public class FileStorageUtil {
+
+    final private String baseStoragePath =
+            "D:\\javaProjects\\JobSync\\src\\main\\java\\Ahmed\\com\\JobSync\\storage\\resumes";
+    private long maxFileSize ;
+
+
+
+    // generate folder for user
+
+    public void createUserDirectory (Long userId) {
+
+    }
+
+    public void generateUserResumePath (Long userId){
+        //Builds standardized file path
+    }
+
+
+
+}
---
+++ src/main/java/Ahmed/com/JobSync/utils/StrageUtils/FileValidationUtil.java
@@ -0,0 +1,5 @@
+package Ahmed.com.JobSync.utils.StrageUtils;
+
+public class FileValidationUtil {
+    // validate file size , file ext , no duplicates files per user
+}
